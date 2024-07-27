69fd67f833ec382358eed0e2cbccc61e0a963dd0
Ivan Trajkovski <trajk.ivan@gmail.com>
2024-07-27 15:40:20 +0200

    testing h1

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1>From Ivan</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
